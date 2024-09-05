ceb24ffbff51eb5398423d35659f677602cc704b
DiegoNotreve <notreveogeide@gmail.com>
2024-09-04 21:58:28 -0300

    aula 3 finalizada

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados= [
+    {
+    item: "Voto dourado",
+    descricao: "O Feitiço Voto Dourado, quando usado, aumentará o ataque e a defesa de você e de qualquer aliado próximo. Isso requer 25 Fé para usar",
+    link: "https://eldenring.wiki.fextralife.com/Golden+Vow+(Spell)"
+},
+    {
+    item: "Moonlight",
+    descricao: "FPCusto: 32, dura 60 segundos, adiciona 65 Dano Mágico e efeito de status de congelamento. Esta Skill é um Buff de Arma , também permitindo ataques pesados ​​para atirar projéteis de onda mágica.",
+    link: "https://eldenring.wiki.fextralife.com/Moonlight+Greatsword"
+},
+    {
+    item: "Uivo de Shabiri",
+    descricao: "Aumento de Dano +25%, Dano recebido +30%. Os inimigos têm mais probabilidade de atacar o conjurador.Duração: 40 segundos.",
+    link: "https://eldenring.wiki.fextralife.com/Moonlight+Greatsword"
+},
+
+    
+];
+
+
+
