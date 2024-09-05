847dce1c13f47748cccc447b34a69b735a01c101
DiegoNotreve <notreveogeide@gmail.com>
2024-09-05 13:56:56 -0300

    adicionei descrição de img no js

--- page.js
+++ page.js
@@ -1,1 +1,21 @@
-console.log(dados);
+function pesquisar(){
+    
+    //console.log(dados); 
+    let section = document.getElementById("resultados-pesquisa")
+/* section.innerHTML = ''; */
+console.log(section);
+let resultados = ""
+for (let dado of dados) {
+    
+    resultados += `
+     <div class="item-resultado">
+     <h2> <a href="#" target="_blank">${dado.item}</a><img src="${dado.img}" alt=""></h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href="${dado.link}">Mais informações</a> 
+                    </div>
+                    
+                    `
+                }
+                
+                section.innerHTML = resultados
+            }
