77992b1a8397bfb1624d92fac95eb02de7f6ec84
DiegoNotreve <notreveogeide@gmail.com>
2024-09-06 14:23:34 -0300

    projeto finalizado?

--- dados.js
+++ dados.js
@@ -3,25 +3,29 @@ let dados = [
         item: "Moonlight",
         descricao: "FPCusto: 32, dura 60 segundos, adiciona 65 Dano Mágico e efeito de status de congelamento. Esta Skill é um Buff de Arma , também permitindo ataques pesados ​​para atirar projéteis de onda mágica.",
         img: "https://img.rankedboost.com/wp-content/plugins/elden-ring/assets/weapons/Dark%20Moon%20Greatsword.png",
-        link: "https://eldenring.wiki.fextralife.com/Moonlight+Greatsword"
+        link: "https://eldenring.wiki.fextralife.com/Moonlight+Greatsword",
+        tags: "espada sword moonlit moonligth moonli"
     },
     {
         item: "Voto dourado",
         descricao: "O Feitiço Voto Dourado, quando usado, aumentará o ataque e a defesa de você e de qualquer aliado próximo. Isso requer 25 Fé para usar",
         img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/golden_vow_incantation_elden_ring_wiki_guide_200px.png",
-        link: "https://eldenring.wiki.fextralife.com/Golden+Vow+(Spell)"
+        link: "https://eldenring.wiki.fextralife.com/Golden+Vow+(Spell)",
+        tags: "dorado buff feitiço feitico dano"
     },
     {
         item: "Uivo de Shabiri",
         img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/howl_of_shabriri_incantation_elden_ring_wiki_guide_200px.png",
         descricao: "Aumento de Dano +25%, Dano recebido +30%. Os inimigos têm mais probabilidade de atacar o conjurador.Duração: 40 segundos.",
-        link: "https://eldenring.wiki.fextralife.com/Howl+of+Shabriri"
+        link: "https://eldenring.wiki.fextralife.com/Howl+of+Shabriri",
+        tags: "ruivo shabiri xabiri buff dano"
     },
     {
         item: "Chama, conceda-me força",
         descricao: "É um encantamento superior dos Monges do Fogo. Tem a capacidade de criar uma chama interna poderosa que amplifica ataques físicos e associados ao fogo, desencadeando capacidades de combate aprimoradas.",
         img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/flame_grant_me%20strength_incantation_elden_ring_wiki_guide_200px.png",
-        link: "https://eldenring.wiki.fextralife.com/Flame+Grant+me+Strength"
+        link: "https://eldenring.wiki.fextralife.com/Flame+Grant+me+Strength",
+        tags: "força consedame fogo"
     }
 ];
 
--- page.js
+++ page.js
@@ -1,21 +1,50 @@
-function pesquisar(){
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
     
-    //console.log(dados); 
-    let section = document.getElementById("resultados-pesquisa")
-/* section.innerHTML = ''; */
-console.log(section);
-let resultados = ""
-for (let dado of dados) {
-    
-    resultados += `
-     <div class="item-resultado">
-     <h2> <a href="#" target="_blank">${dado.item}</a><img src="${dado.img}" alt=""></h2>
-                    <p class="descricao-meta">${dado.descricao}</p>
-                    <a href="${dado.link}">Mais informações</a> 
-                    </div>
-                    
-                    `
-                }
-                
-                section.innerHTML = resultados
-            }
+
+    //Aqui faz com que quando o botão de pesquisar for apertado sem escrever nada no input, ele retorna a frase "nenhum resultado"
+    if (campoPesquisa == ""){
+        section.innerHTML = "<p>nenhum resultado<p/>"
+        return
+    }
+
+    //Independente do usuário escrever com letra minúscula uo maiúscula, vai funcionar
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let item = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+        item = dado.item.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        descricao = dado.tags.toLowerCase()
+        
+        if (item.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            //cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.item}</a>
+                    <img src=${dado.img} alt="">
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
+        }
+       if(!resultados){
+        resultados = "<p>Esse item não existe na base de dados</p>"
+       } 
+      
+    }
+
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+}
