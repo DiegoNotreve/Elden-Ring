b89bd1b4b60a3c47531c7bbc80bf9e0ce29320c2
DiegoNotreve <notreveogeide@gmail.com>
2024-09-07 00:26:42 -0300

    finalmente finalizado

--- dados.js
+++ dados.js
@@ -1,37 +1,128 @@
 let dados = [
-    {
-        item: "Moonlight",
-        descricao: "FPCusto: 32, dura 60 segundos, adiciona 65 Dano Mágico e efeito de status de congelamento. Esta Skill é um Buff de Arma , também permitindo ataques pesados ​​para atirar projéteis de onda mágica.",
-        img: "https://img.rankedboost.com/wp-content/plugins/elden-ring/assets/weapons/Dark%20Moon%20Greatsword.png",
-        link: "https://eldenring.wiki.fextralife.com/Moonlight+Greatsword",
-        linksite: "https://static1.srcdn.com/wordpress/wp-content/uploads/2024/03/untitled-design-47-2.jpg",
-        tags: "espada sword moonlit moonligth moonli"
-    },
-    {
-        item: "Voto dourado",
-        descricao: "O Feitiço Voto Dourado, quando usado, aumentará o ataque e a defesa de você e de qualquer aliado próximo. Isso requer 25 Fé para usar",
-        img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/golden_vow_incantation_elden_ring_wiki_guide_200px.png",
-        link: "https://eldenring.wiki.fextralife.com/Golden+Vow+(Spell)",
-        linksite: "https://static1.srcdn.com/wordpress/wp-content/uploads/2022/04/Elden-Ring-Golden-Vow-Spell-Showcase.jpg",
-        tags: "dorado buff feitiço feitico dano"
-    },
-    {
-        item: "Uivo de Shabiri",
-        img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/howl_of_shabriri_incantation_elden_ring_wiki_guide_200px.png",
-        descricao: "Aumento de Dano +25%, Dano recebido +30%. Os inimigos têm mais probabilidade de atacar o conjurador.Duração: 40 segundos.",
-        link: "https://eldenring.wiki.fextralife.com/Howl+of+Shabriri",
-        linksite:"https://imgur.com/tYEer32",
-        tags: "ruivo shabiri xabiri buff dano"
-    },
-    {
-        item: "Chama, conceda-me força",
-        descricao: "É um encantamento superior dos Monges do Fogo. Tem a capacidade de criar uma chama interna poderosa que amplifica ataques físicos e associados ao fogo, desencadeando capacidades de combate aprimoradas.",
-        img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/flame_grant_me%20strength_incantation_elden_ring_wiki_guide_200px.png",
-        link: "https://eldenring.wiki.fextralife.com/Flame+Grant+me+Strength",
-        linksite:"https://i.ytimg.com/vi/8afuh7Lxvc4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCerZo8AX-W8pvFYOzN4gUZMaeupw",
-        tags: "força consedame fogo"
-    }
+  {
+    item: "Moonlight",
+    descricao:
+      "FPCusto: 32, dura 60 segundos, adiciona 65 Dano Mágico e efeito de status de congelamento. Esta Skill é um Buff de Arma , também permitindo ataques pesados ​​para atirar projéteis de onda mágica.",
+    img: "https://img.rankedboost.com/wp-content/plugins/elden-ring/assets/weapons/Dark%20Moon%20Greatsword.png",
+    link: "https://eldenring.wiki.fextralife.com/Moonlight+Greatsword",
+    linksite:
+      "https://static1.srcdn.com/wordpress/wp-content/uploads/2024/03/untitled-design-47-2.jpg",
+  },
+  {
+    item: "Voto dourado",
+    descricao:
+      "O Feitiço Voto Dourado, quando usado, aumentará o ataque e a defesa de você e de qualquer aliado próximo. Isso requer 25 Fé para usar",
+    img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/golden_vow_incantation_elden_ring_wiki_guide_200px.png",
+    link: "https://eldenring.wiki.fextralife.com/Golden+Vow+(Spell)",
+    linksite:
+      "https://static1.srcdn.com/wordpress/wp-content/uploads/2022/04/Elden-Ring-Golden-Vow-Spell-Showcase.jpg",
+  },
+  {
+    item: "Uivo de Shabiri",
+    img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/howl_of_shabriri_incantation_elden_ring_wiki_guide_200px.png",
+    descricao:
+      "Aumento de Dano +25%, Dano recebido +30%. Os inimigos têm mais probabilidade de atacar o conjurador.Duração: 40 segundos.",
+    link: "https://eldenring.wiki.fextralife.com/Howl+of+Shabriri",
+    linksite: "https://imgur.com/tYEer32",
+  },
+  {
+    item: "Chama, conceda-me força",
+    descricao:
+      "É um encantamento superior dos Monges do Fogo. Tem a capacidade de criar uma chama interna poderosa que amplifica ataques físicos e associados ao fogo, desencadeando capacidades de combate aprimoradas.",
+    img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/flame_grant_me%20strength_incantation_elden_ring_wiki_guide_200px.png",
+    link: "https://eldenring.wiki.fextralife.com/Flame+Grant+me+Strength",
+    linksite:
+      "https://i.ytimg.com/vi/8afuh7Lxvc4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCerZo8AX-W8pvFYOzN4gUZMaeupw",
+  },
+  /* -----------------adição de itens------------------- */
+
+  {
+    item: "Foice Alada",
+    descricao:
+      "A Foice Alada é uma arma do tipo ceifador em Elden Ring que causa dano sagrado e também nega dano sagrado recebido. Você pode encontrar essa arma enquanto ainda está em Limgrave, então vale a pena adquiri-la o quanto antes.",
+    img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/winged_scythe_reaper_weapon_elden_ring_wiki_guide_200px.png",
+    link: "https://eldenring.wiki.fextralife.com/Winged+Scythe",
+    linksite:
+      "https://criticalhits.com.br/wp-content/uploads/2024/04/elden-ring-winged-scythe-1280x640.webp",
+  },
+  {
+    item: "Rios de Sangue",
+    descricao:
+      "Rios de Sangue é ideal para aqueles que possuem build com foco em Destreza, visto que ela é a arma mais forte do jogo neste atributo. A passiva da espada permite tirar pedaços consideráveis da vida dos inimigos com golpes sucessivos, facilitando consideravelmente alguns encontros desafiadores",
+    img: "https://cdn.ome.lt/csCQ_oLfLt51IxmQKbGpEcgH1Zk=/770x0/smart/uploads/conteudo/fotos/elden_ring_rivers_of_blood_png.webp",
+    link: "https://criticalhits.com.br/games/elden-ring-onde-encontrar-a-katana-rios-de-sangue/",
+    linksite:
+      "https://overplay.com.br/wp-content/uploads/2022/03/rios-sangue-elden-ring-overplay.webp",
+  },
+  {
+    item: "lança relâmpago de Fortissax",
+    descricao:
+      "Cria duas lanças de relâmpago vermelho e as crava no chão de cima, uma após a outra. No impacto, cada lança explodirá em trilhas de relâmpago cobrindo a área. Requer 46 de fé.",
+    img: "https://img.game8.co/3498530/ae705f2f113b00a774cea6632895d84f.png/show",
+    link: "https://game8.co/games/Elden-Ring/archives/360635",
+    linksite: "https://i.ytimg.com/vi/KJmybxBQ3M8/maxresdefault.jpg",
+  },
+  {
+    item: "Lâmina Furiosa de Ansbach",
+    descricao:
+      "Corta inimigos com uma lâmina de chamas de sangue invocada na lateral da mão. Requer 27 arcano e 19 de fé.",
+    img: "https://republicadg.com.br/wp-content/uploads/2024/07/image-94.png",
+    link: "https://eldenring.wiki.fextralife.com/Furious+Blade+of+Ansbach",
+    linksite:
+      "https://i1.wp.com/gameswfu.net/imgiv4-gpj.cip-dna-esu-hcabsna-fo-edalb-suoiruf-gnir-nedle/70/4202/sdaolpu/tnetnoc-pw/sserpdrow/moc.segamitnaremag.0citats.jpeg",
+  },
+  {
+    item: "Conhecimento da Lua Cheia de Rennala",
+    descricao:
+      "Usa o conjurador como um recipiente para encarnar uma lua cheia, então a envia flutuando em direção aos inimigos. A lua cheia dissipa toda a feitiçaria que a toca, e reduz temporariamente a negação de dano mágico para aqueles que ela atinge. Requer 70 Inteligência.",
+    img: "https://img.game8.co/3494544/a14097d53c86b3d13e039afba3f14bce.png/show",
+    link: "https://game8.co/games/Elden-Ring/archives/369541",
+    linksite:
+      "https://static1.srcdn.com/wordpress/wp-content/uploads/2022/03/How-To-Get-Rannis-Dark-Moon-In-Elden-Ring.jpg",
+  },
+  {
+    item: "Espada Mágica de Carian",
+    descricao:
+      "Um feitiço que invoca uma espada de luz mágica para desferir um poderoso ataque de corte. Requer 24 Inteligência.",
+    img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/adulas_moonblade_sorcery_elden_ring_wiki_guide_200px.png",
+    link: "https://game8.co/games/Elden-Ring/archives/369743",
+    linksite:
+      "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/04/adulas-Moonblade-in-elden-ring.jpg",
+  },
+  {
+    item: "Maestria de Loretta",
+    descricao:
+      "Cria um arco mágico e dispara quatro flechas grandes simultaneamente. Carregar aumenta a potência. Segure para manter as flechas grandes engatadas. Requer 46 de Inteligência.",
+    img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/lorettas_mastery_sorcery_elden_ring_wiki_guide_200px.png",
+    link: "https://game8.co/games/Elden-Ring/archives/372968",
+    linksite:
+      "https://eldenring.wiki.fextralife.com/file/Elden-Ring/lorettas_mastery_sorcery_elden_ring_wiki_guide_200px.png",
+  },
+  {
+    item: "Meteorito de Astel",
+    descricao:
+      "Convoca uma chuva de meteoritos que causa enorme dano de área em um grande raio. Requer 55 Inteligência.",
+    img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/meteorite_of_astel_sorcery_elden_ring_wiki_guide_200px.png",
+    link: "https://game8.co/games/Elden-Ring/archives/372964",
+    linksite:
+      "https://preview.redd.it/h45mw1ug6dt81.jpg?auto=webp&s=8dc280cbae4534ab1b33f649d86abb708ffaa827",
+  },
+  {
+    item: "Fragmento da noite",
+    descricao:
+      "Dispara rapidamente um projétil semi-invisível. Esta feitiçaria pode ser usada sem demora após executar outra ação.",
+    img: "https://img.game8.co/3495560/aca90ee896c5f63cafbbd4c185324257.png/show",
+    link: "https://game8.co/games/Elden-Ring/archives/369753",
+    linksite:
+      "https://img.game8.co/3495560/aca90ee896c5f63cafbbd4c185324257.png/show",
+  },
+  {
+    item: "Cometa Azur",
+    descricao:
+      "Dispara um tremendo cometa em uma torrente semelhante à distante expansão estrelada, o lugar que dizem ser a origem da pedra brilhante. Segure para continuar liberando o poder da feitiçaria.",
+    img: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/comet_azur_sorcery_elden_ring_wiki_guide_200px.png",
+    link: "https://game8.co/games/Elden-Ring/archives/369992",
+    linksite:
+      "https://static1.srcdn.com/wordpress/wp-content/uploads/2022/03/Elden-Ring-How-To-Get-Comet-Azur-Sorcery-Spell.jpg",
+  },
 ];
-
-
-
--- page.js
+++ page.js
@@ -1,34 +1,51 @@
+document.body.addEventListener("click", function () {
+  var audio = document.getElementById("background-audio");
+  if (audio.paused) {
+    audio.play();
+  }
+});
+
 function pesquisar() {
-    // Obtém a seção HTML onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
+  // Obtém a seção HTML onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
 
-    let campoPesquisa = document.getElementById("campo-pesquisa").value
-    
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
-    //Aqui faz com que quando o botão de pesquisar for apertado sem escrever nada no input, ele retorna a frase "nenhum resultado"
-    if (!campoPesquisa){
-        section.innerHTML = "<p class='retorno'>Nenhum resultado ⚠ <p/>";
-        return
-    }
+  //Aqui faz com que quando o botão de pesquisar for apertado sem escrever nada no input, ele retorna a frase "nenhum resultado"
+  if (!campoPesquisa) {
+    section.innerHTML = "<p class='retorno'>Nenhum resultado ⚠ <p/>";
+    return;
+  }
 
-    //Independente do usuário escrever com letra minúscula uo maiúscula, vai funcionar
-    campoPesquisa = campoPesquisa.toLowerCase()
+  //Independente do usuário escrever com letra minúscula uo maiúscula, vai funcionar
+  campoPesquisa = campoPesquisa.toLowerCase()
+  .normalize("NFD")
+  .replace(/[\u0300-\u036f]/g, "");
 
-    // Inicializa uma string vazia para armazenar os resultados
-    let resultados = "";
-    let item = "";
-    let descricao = "";
-    let tags = "";
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = "";
+  let item = "";
+  let descricao = "";
+  
 
-    // Itera sobre cada dado da lista de dados
-    for (let dado of dados) {
-        item = dado.item.toLowerCase()
-        descricao = dado.descricao.toLowerCase()
-        descricao = dado.tags.toLowerCase()
-        
-        if (item.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
-            //cria um novo elemento
-            resultados += `
+  // Itera sobre cada dado da lista de dados
+  for (let dado of dados) {
+    item = dado.item
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");/* este comando de normalize ignora acentuação */
+    descricao = dado.descricao
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+
+    if (
+      item.includes(campoPesquisa) ||
+      descricao.includes(campoPesquisa)
+      
+    ) {
+      //cria um novo elemento
+      resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href=${dado.linksite} target="_blank">${dado.item}</a>
@@ -38,13 +55,13 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
         `;
-        }
-        
     }
-    
-    if(!resultados){
-     resultados = "<p class='retorno'>Esse item não existe na base de dados ⚠</p>" ;
-    } 
-    // Atribui os resultados gerados à seção HTML
-    section.innerHTML = resultados;
+  }
+
+  if (!resultados) {
+    resultados =
+      "<p class='retorno'>Esse item não existe na base de dados ⚠</p>";
+  }
+  // Atribui os resultados gerados à seção HTML
+  section.innerHTML = resultados;
 }
